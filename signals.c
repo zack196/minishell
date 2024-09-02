ce9061a04f8bf409e6a58da5418f735c6219425c
nihalamrene <96698547+nihalamrene@users.noreply.github.com>
2024-09-02 21:05:29 +0100

    Update signals.c

--- signals.c
+++ signals.c
@@ -1,5 +1,5 @@
 #include "includes/parse.h"
-
+//normal shell 
 void	sig_int_handler(int s)
 {
 	(void)s;
@@ -8,15 +8,32 @@ void	sig_int_handler(int s)
 	rl_on_new_line();
 	rl_redisplay();
 }
-
-void	set_signals(void)
+void	set_signals(t_envi	*dup_env)
 {
 	signal(SIGQUIT, SIG_IGN);
 	signal(SIGINT, sig_int_handler);
+	set_exit_status(1,&dup_env);
 }
 
-void	set_default(void)
+//in prrocess
+void	sig_quit_handler(int s)
 {
-	signal(SIGQUIT, SIG_DFL);
-	signal(SIGINT, SIG_DFL);
+	(void)s;
+	ft_putstr_fd("Quit : 3\n",1);
+}
+void	set_child_process(void)
+{
+	signal(SIGQUIT,sig_quit_handler);
+}
+//heredoc
+static void handler_heredoc(int sig)
+{
+
+    (void)sig;
+   g_heredoc_interrupted = 6;
+   ioctl(STDIN_FILENO, TIOCSTI, "\n");
+}
+void	set_herdoc(void)
+{
+	signal(SIGINT, handler_heredoc);
 }
